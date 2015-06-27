045775730a9979a10fd33caa92de3b2aaa0559de
niorgai <jianqiu.sysu@gmail.com>
2015-06-28 01:58:11 +0800

    change to AppBarLayout & TabLayout

--- app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
+++ app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package us.wili.qtwallpaper.activity;
 
-import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import java.util.HashMap;
 
@@ -15,67 +16,68 @@ import us.wili.qtwallpaper.R;
 import us.wili.qtwallpaper.fragment.CategoryFragment;
 import us.wili.qtwallpaper.fragment.HotFragment;
 import us.wili.qtwallpaper.fragment.PersonalFragment;
-import us.wili.qtwallpaper.utils.UIUtils;
 
 
-public class MainActivity extends FragmentActivity implements RadioGroup.OnCheckedChangeListener{
+public class MainActivity extends AppCompatActivity {
     public static final int HOT_PAGE = 0;
     public static final int CATEGORY_PAGE = 1;
     public static final int PERSONAL_PAGE = 2;
     private HashMap<Integer,Fragment> fragments = new HashMap<>();
-    private int fragmentContentId;
-    private int currentTab;
-    private boolean isExit = false;
+    private HashMap<Integer,String> fragmentsTitles = new HashMap<>();
 
-    private RadioGroup menuGroup;
+    private Toolbar toolbar;
+    private AppBarLayout appbar;
+    private ViewPager viewpager;
+    private TabLayout tabs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        menuGroup = (RadioGroup)findViewById(R.id.tab_group);
-        fragmentContentId = R.id.content;
 
-        fragments.put(HOT_PAGE, new HotFragment());
-        fragments.put(CATEGORY_PAGE, new CategoryFragment());
-        fragments.put(PERSONAL_PAGE, new PersonalFragment());
-        FragmentTransaction ft = MainActivity.this.getSupportFragmentManager().beginTransaction();
-        ft.add(fragmentContentId, fragments.get(HOT_PAGE));
-        currentTab = HOT_PAGE;
-        ft.commit();
-        menuGroup.setOnCheckedChangeListener(this);
+        initialize();
+
+        setSupportActionBar(toolbar);
+
+        viewpager.setAdapter(new MainAdapter(getSupportFragmentManager()));
+        tabs.setupWithViewPager(viewpager);
     }
 
-    private void showTab(int index){
 
-        FragmentTransaction ft = MainActivity.this.getSupportFragmentManager().beginTransaction();
-        ft.hide(fragments.get(currentTab));
-        ft.show(fragments.get(index));
-        ft.commit();
-        menuGroup.setOnCheckedChangeListener(null);
-        ((RadioButton)menuGroup.getChildAt(currentTab)).setChecked(false);
-        currentTab = index;
-        ((RadioButton)menuGroup.getChildAt(currentTab)).setChecked(true);
-        menuGroup.setOnCheckedChangeListener(this);
+    private void initialize() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        appbar = (AppBarLayout) findViewById(R.id.appbar);
+        viewpager = (ViewPager) findViewById(R.id.viewpager);
+        tabs = (TabLayout) findViewById(R.id.tabs);
     }
 
-    public void changeTab(int index){
-        ((RadioButton)menuGroup.getChildAt(index)).setChecked(true);
-    }
+    private class MainAdapter extends FragmentPagerAdapter {
 
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        for(int i=0; i<group.getChildCount(); i++){
-            if(group.getChildAt(i).getId() == checkedId){
-                Fragment fragment = fragments.get(i);
-                FragmentTransaction ft = MainActivity.this.getSupportFragmentManager().beginTransaction();
-                if(!fragment.isAdded()){
-                    ft.add(fragmentContentId,fragment);
-                }
-                showTab(i);
-                ft.commit();
-            }
+        public MainAdapter(FragmentManager fm) {
+            super(fm);
+            fragments.put(HOT_PAGE, new HotFragment());
+            fragments.put(CATEGORY_PAGE, new CategoryFragment());
+            fragments.put(PERSONAL_PAGE, new PersonalFragment());
+            fragmentsTitles.put(HOT_PAGE, getString(R.string.hot));
+            fragmentsTitles.put(CATEGORY_PAGE, getString(R.string.category));
+            fragmentsTitles.put(PERSONAL_PAGE, getString(R.string.personal));
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentsTitles.get(position);
         }
     }
 }
