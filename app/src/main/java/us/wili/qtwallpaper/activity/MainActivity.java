3d1ebcccb56667296caed7c162fb06410a55cd2d
niorgai <jianqiu.sysu@gmail.com>
2015-07-10 15:00:03 +0800

    delete toolbar

--- app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
+++ app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
@@ -20,7 +20,7 @@ import us.wili.qtwallpaper.fragment.HotFragment;
 import us.wili.qtwallpaper.fragment.PersonalFragment;
 
 
-public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
+public class MainActivity extends AppCompatActivity{
     public static final int HOT_PAGE = 0;
     public static final int CATEGORY_PAGE = 1;
     public static final int PERSONAL_PAGE = 2;
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
 
     private ViewPager viewpager;
     private TabLayout tabs;
-    private TextView title;
 
 
     @Override
@@ -42,30 +41,12 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
 
         viewpager.setAdapter(new MainAdapter(getSupportFragmentManager()));
         tabs.setupWithViewPager(viewpager);
-        tabs.setOnTabSelectedListener(this);
-        title.setText(R.string.hot);
     }
 
 
     private void initialize() {
         viewpager = (ViewPager) findViewById(R.id.viewpager);
         tabs = (TabLayout) findViewById(R.id.tabs);
-        title = (TextView) findViewById(R.id.title);
-    }
-
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        title.setText(fragmentsTitles.get(tab.getPosition()));
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
     }
 
     private class MainAdapter extends FragmentPagerAdapter {
